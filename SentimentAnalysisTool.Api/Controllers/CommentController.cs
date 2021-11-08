f4662fe2d580de05eafaa0820da30cd3e7d3da36
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-08 17:16:07 +0800

    Added CommentController

---
+++ SentimentAnalysisTool.Api/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Api.Controllers
+{
+    public class CommentController : ControllerBase
+    {        
+        public CommentController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadCsv(IFormFile csvFormFile)
+        {
+            var filePath = Path.GetTempFileName();
+            if (csvFormFile.Length > 0)
+            {
+                using FileStream fileStream = new FileStream(filePath, FileMode.Create);
+                await csvFormFile.CopyToAsync(fileStream);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
