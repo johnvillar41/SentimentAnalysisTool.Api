5912ebc33013edf7c747d29e490c3607b03269ed
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-12 08:34:53 +0800

    Added Services

---
+++ SentimentAnalysisTool.Services/Services/Implementations/CommentService.cs
@@ -0,0 +1,8 @@
+﻿using SentimentAnalysisTool.Services.Services.Interfaces;
+
+namespace SentimentAnalysisTool.Services.Services.Implementations
+{
+    public class CommentService : ICommentService
+    {
+    }
+}
---
+++ SentimentAnalysisTool.Services/Services/Interfaces/ICommentService.cs
@@ -0,0 +1,6 @@
+﻿namespace SentimentAnalysisTool.Services.Services.Interfaces
+{
+    public interface ICommentService
+    {
+    }
+}
