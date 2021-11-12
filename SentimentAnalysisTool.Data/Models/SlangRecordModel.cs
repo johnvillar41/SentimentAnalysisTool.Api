56d0a0a0cb5a497298b15c994d1ee4bf4114ca08
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-12 19:52:25 +0800

    Added SlangRecordModel

---
+++ SentimentAnalysisTool.Data/Models/SlangRecordModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SentimentAnalysisTool.Data.Models
+{
+    public class SlangRecordModel
+    {
+        public int SlangRecordId { get; set; }
+        public RecordModel Record { get; set; }
+        public string SlangName { get; set; }
+    }
+}
