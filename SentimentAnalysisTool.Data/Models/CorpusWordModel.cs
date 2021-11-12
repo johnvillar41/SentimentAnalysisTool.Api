6d16e5c45b13dbdf4ee5db875367279585a50cb6
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-12 19:56:09 +0800

    Updated CorpusWordModel

--- SentimentAnalysisTool.Data/Models/CorpusWordModel.cs
+++ SentimentAnalysisTool.Data/Models/CorpusWordModel.cs
@@ -5,8 +5,7 @@ namespace SentimentAnalysisTool.Data.Models
     public class CorpusWordModel
     {
         public int CorpusWordId { get; set; }
-        public string CorpusWord { get; set; }
-
         public CorpusTypeModel CorpusType { get; set; }
+        public string CorpusWord { get; set; }
     }
 }
