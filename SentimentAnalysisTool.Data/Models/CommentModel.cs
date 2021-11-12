948572a836ba9df314c9ccc8a6bceec89980b254
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-12 19:48:50 +0800

    Updated CommentModel

--- SentimentAnalysisTool.Data/Models/CommentModel.cs
+++ SentimentAnalysisTool.Data/Models/CommentModel.cs
@@ -6,10 +6,9 @@ namespace SentimentAnalysisTool.Data.Models
     public class CommentModel
     {
         public int CommentId { get; set; }
+        public RecordModel Record { get; set; }
         public int CommentScore { get; set; }
         public string CommentDetail { get; set; }
         public DateTime Date { get; set; }
-
-        public RecordModel Record { get; set; }
     }
 }
