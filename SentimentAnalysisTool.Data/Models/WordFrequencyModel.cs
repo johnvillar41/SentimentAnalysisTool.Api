f097c9239eeddd9ede0710e68cac92a5a4650e20
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-12 19:53:58 +0800

    Updated WordFrequencyModel

--- SentimentAnalysisTool.Data/Models/WordFrequencyModel.cs
+++ SentimentAnalysisTool.Data/Models/WordFrequencyModel.cs
@@ -3,9 +3,10 @@
 namespace SentimentAnalysisTool.Data.Models
 {
     public class WordFrequencyModel
-    { 
+    {
         public int WordFrequencyId { get; set; }
+        public RecordModel Record { get; set; }
         public string Word { get; set; }
-        public int WordFrequency { get; set; }        
+        public int WordFrequency { get; set; }
     }
 }
