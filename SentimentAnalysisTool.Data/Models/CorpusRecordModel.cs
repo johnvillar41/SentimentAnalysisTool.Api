d128ef9e05209fbc9bda65758c31d0528fa25be0
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-12 19:50:38 +0800

    Updated CorpusRecordModel

--- SentimentAnalysisTool.Data/Models/CorpusRecordModel.cs
+++ SentimentAnalysisTool.Data/Models/CorpusRecordModel.cs
@@ -6,9 +6,8 @@ namespace SentimentAnalysisTool.Data.Models
     public class CorpusRecordModel
     {
         public int CorpusRecordId { get; set; }
-        public string CorpusName { get; set; }
-
-        public IEnumerable<CorpusTypeModel> CorpusTypes { get; set; }
         public RecordModel Record { get; set; }
+        public string CorpusName { get; set; }
+        public IEnumerable<CorpusTypeModel> CorpusTypes { get; set; }
     }
 }
