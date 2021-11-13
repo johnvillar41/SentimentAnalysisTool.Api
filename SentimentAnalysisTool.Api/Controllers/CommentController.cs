c9df639454395c17ca5140c359c263b83947979c
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-13 20:02:12 +0800

    Updated Comment Controller

--- SentimentAnalysisTool.Api/Controllers/CommentController.cs
+++ SentimentAnalysisTool.Api/Controllers/CommentController.cs
@@ -1,20 +1,43 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace SentimentAnalysisTool.Api.Controllers
 {
     public class CommentController : ControllerBase
-    {        
-        public CommentController()
+    {
+        private readonly ICommentService _commentService;
+        private readonly IConfiguration _configuration;
+        private string ConnectionString { get; set; }
+        public CommentController(ICommentService commentService, IConfiguration configuration)
         {
+            _commentService = commentService;
+            _configuration = configuration;
+            ConnectionString = _configuration.GetConnectionString("SentimentDBConnection");
+        }
 
-        }
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> FetchComments(int pageSize = 10, int pageNumber = 1)
         {
-            return Ok();
+            var comments = await _commentService.FetchCommentsAsync(pageSize, pageNumber, ConnectionString);
+            if (((List<CommentModel>)comments).Count == 0)
+                return NoContent();
+
+            return Ok(comments);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveComments()
+        {
+            throw new NotImplementedException();
+        }
+
         [HttpPost]
         public async Task<IActionResult> UploadCsv(IFormFile csvFormFile)
         {
