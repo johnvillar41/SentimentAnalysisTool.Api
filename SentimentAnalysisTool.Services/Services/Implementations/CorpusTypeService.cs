11e80c2d3b8bec31c36d1032f6c67e4355eefd78
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-13 17:36:10 +0800

    Added CorpusTypeService

---
+++ SentimentAnalysisTool.Services/Services/Implementations/CorpusTypeService.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Services.Implementations
+{
+    public class CorpusTypeService : ICorpusTypeService
+    {
+        public async Task<bool> AddCorpusTypeAsync(CorpusTypeModel corpusType, string connectionString)
+        {
+            var sqlQuery = @"INSERT INTO CorpusTypeTable(CorpusRecordsId,CorpusTypeName)
+                            VALUES(
+                                @CorpusRecordsId,
+                                @CorpusTypeName
+                            )";
+            using SqlConnection connection = new SqlConnection(connectionString);
+            await connection.OpenAsync();
+            var transaction = await connection.BeginTransactionAsync();
+            var rowsAffected = await connection.ExecuteAsync(sqlQuery, corpusType, transaction);
+            await transaction.CommitAsync();
+            if (rowsAffected > 0)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ SentimentAnalysisTool.Services/Services/Interfaces/ICorpusTypeService.cs
@@ -0,0 +1,14 @@
+﻿using SentimentAnalysisTool.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Services.Interfaces
+{
+    public interface ICorpusTypeService
+    {
+        Task<bool> AddCorpusTypeAsync(CorpusTypeModel corpusType, string connectionString);
+    }
+}
