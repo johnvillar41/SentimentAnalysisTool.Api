301e58f2a45e390f692596cc4745f7f100302ac1
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-13 08:18:30 +0800

    Updated CommentService

--- SentimentAnalysisTool.Services/Services/Implementations/CommentService.cs
+++ SentimentAnalysisTool.Services/Services/Implementations/CommentService.cs
@@ -1,8 +1,36 @@
-﻿using SentimentAnalysisTool.Services.Services.Interfaces;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Services.Interfaces;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace SentimentAnalysisTool.Services.Services.Implementations
 {
     public class CommentService : ICommentService
     {
+        public async Task<bool> SaveComments(IEnumerable<CommentModel> comments, string connectionString)
+        {
+            using SqlConnection connection = new SqlConnection(connectionString);
+            await connection.OpenAsync();
+            IDbTransaction transaction = connection.BeginTransaction();
+            var insertedRows = 0;
+            foreach(var comment in comments)
+            {
+                insertedRows = await connection.ExecuteAsync("INSERT INTO CommentsTable(RecordId,CommentScore,CommentDetail,Date) VALUES(@RecordId,@CommentScore,@CommentDetail,@Date)",
+                    new { 
+                        RecordId = comment.Record.RecordId,
+                        CommentScore = comment.CommentScore,
+                        CommentDetail = comment.CommentDetail,
+                        Date = comment.Date
+                    });
+            }
+            if (insertedRows > 0)
+                return true;
+
+            return false;
+        }
     }
 }
--- SentimentAnalysisTool.Services/Services/Interfaces/ICommentService.cs
+++ SentimentAnalysisTool.Services/Services/Interfaces/ICommentService.cs
@@ -1,6 +1,12 @@
-﻿namespace SentimentAnalysisTool.Services.Services.Interfaces
+﻿using Microsoft.AspNetCore.Http;
+using SentimentAnalysisTool.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Services.Interfaces
 {
     public interface ICommentService
     {
+        Task<bool> SaveComments(IEnumerable<CommentModel> comments, string connectionString);
     }
 }
