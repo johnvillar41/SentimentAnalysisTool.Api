50fcb0ac6b75a08228c7fdcad365165cfaa8277c
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-16 14:53:55 +0800

    Added CorpusRecordsController

---
+++ SentimentAnalysisTool.Api/Controllers/CorpusRecordsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SentimentAnalysisTool.Api.Models;
+using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CorpusRecordsController : ControllerBase
+    {
+        private readonly ICorpusRecordService _corpusRecordService;
+        private readonly IRecordService _recordService;
+        private readonly IConfiguration _configuration;
+        private string ConnectionString { get; }
+        public CorpusRecordsController(
+            ICorpusRecordService corpusRecordService,
+            IRecordService recordService,
+            IConfiguration configuration)
+        {
+            _corpusRecordService = corpusRecordService;
+            _recordService = recordService;
+            _configuration = configuration;
+            ConnectionString = _configuration.GetConnectionString("SentimentDBConnection");
+        }
+        //POST: api/CorpusRecords
+        [HttpPost]
+        public async Task<IActionResult> AddCorpusRecord([FromBody] CorpusRecordViewModel corpusRecordViewModel)
+        {
+            if (corpusRecordViewModel == null)
+                return NotFound("Empty Corpus!");
+
+            var corpusRecordModel = new CorpusRecordModel()
+            {
+                CorpusRecordId = -1,
+                Record = await _recordService.FindRecordAsync(corpusRecordViewModel.RecordId, ConnectionString),
+                CorpusName = corpusRecordViewModel.CorpusName,
+                CorpusTypes = null
+            };
+            var result = await _corpusRecordService.AddCorpusRecordAsync(corpusRecordModel, ConnectionString);
+            if (result)
+                return Ok();
+
+            return BadRequest();
+        }
+    }
+}
---
+++ SentimentAnalysisTool.Tests/CorpusRecordsControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using SentimentAnalysisTool.Api.Controllers;
+using SentimentAnalysisTool.Api.Models;
+using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SentimentAnalysisTool.Tests
+{
+    public class CorpusRecordsControllerTests
+    {
+        private readonly Mock<ICorpusRecordService> mockCorpusRecordService;
+        private readonly Mock<IRecordService> mockRecordService;
+        private readonly Mock<IConfiguration> mockConfiguration;
+        private readonly CorpusRecordsController corpusRecordsController;
+        public CorpusRecordsControllerTests()
+        {
+            mockCorpusRecordService = new Mock<ICorpusRecordService>();
+            mockRecordService = new Mock<IRecordService>();
+            mockConfiguration = new Mock<IConfiguration>();
+            corpusRecordsController = new CorpusRecordsController(
+                mockCorpusRecordService.Object,
+                mockRecordService.Object,
+                mockConfiguration.Object);
+        }
+        [Fact]
+        public async Task Should_Return_NotFound_When_CorpusRecordViewModel_Is_Null()
+        {
+            //Act
+            var result = await corpusRecordsController.AddCorpusRecord(null);
+            //Assert
+            var message = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("Empty Corpus!", message.Value);
+        }
+        [Fact]
+        public async Task Should_Return_Ok_When_AddCorpusRecordAsync_Returns_True()
+        {
+            //Arrange
+            mockCorpusRecordService
+                .Setup(m => m.AddCorpusRecordAsync(It.IsAny<CorpusRecordModel>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(true));
+            //Act
+            var result = await corpusRecordsController.AddCorpusRecord(Mock.Of<CorpusRecordViewModel>());
+            //Assert
+            Assert.IsType<OkResult>(result);
+        }
+        [Fact]
+        public async Task Should_Return_BadRequest_When_AddCorpusRecordAsync_Returns_False()
+        {
+            //Arrange
+            mockCorpusRecordService
+                .Setup(m => m.AddCorpusRecordAsync(It.IsAny<CorpusRecordModel>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(false));
+            //Act
+            var result = await corpusRecordsController.AddCorpusRecord(Mock.Of<CorpusRecordViewModel>());
+            //Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+    }
+}
