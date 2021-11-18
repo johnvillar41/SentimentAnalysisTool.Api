4f08318f0f915002637e42a698a8751a50359805
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-18 14:39:33 +0800

    Updated FileHelper

--- SentimentAnalysisTool.Api/Helpers/FileHelper.cs
+++ SentimentAnalysisTool.Api/Helpers/FileHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using SentimentAnalysisTool.Data.Models;
 using System;
 using System.Collections.Generic;
@@ -10,20 +11,35 @@ namespace SentimentAnalysisTool.Api.Helpers
 {
     public class FileHelper : IFileHelper
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public FileHelper(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
         public async Task<bool> UploadCsv(IFormFile csvFormFile)
         {
-            var filePath = Path.GetTempFileName();
-            if (csvFormFile.Length > 0)
-            {
-                using FileStream fileStream = new FileStream(filePath, FileMode.Create);
-                await csvFormFile.CopyToAsync(fileStream);
-                return true;
-            }
-            return false;
+            var filePath = await BuildCsvLink(csvFormFile);
+            if (filePath == null)
+                return false;
+
+            return true;
         }
         public async Task<ICollection<CommentModel>> PolarizeCsvFile(IFormFile csvFormFile)
         {
             throw new NotImplementedException();
+        }
+        private async Task<string> BuildCsvLink(IFormFile csvFormFile)
+        {
+            var fileExtension = Path.GetExtension(csvFormFile.FileName);
+            var guid = Guid.NewGuid();
+            if (fileExtension.Equals(".xslx", StringComparison.CurrentCultureIgnoreCase))
+            {
+                var saveFile = Path.Combine(_webHostEnvironment.WebRootPath, @"files\", $"{guid}{csvFormFile.FileName}");
+                var stream = new FileStream(saveFile, FileMode.Create);
+                await csvFormFile.CopyToAsync(stream);
+                return @$"images\customers\{guid}{csvFormFile.FileName}";
+            }
+            return null;
         }
     }
 }
