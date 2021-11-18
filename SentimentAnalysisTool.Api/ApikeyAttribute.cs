269d3d48d354e01023eb0a0ac5d1e68b006419c5
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-18 14:45:42 +0800

    Added ApikeyAttribute class

---
+++ SentimentAnalysisTool.Api/ApikeyAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Api
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApikeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string APIKEY_HEADER_NAME = "Apikey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(APIKEY_HEADER_NAME, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apikey = configuration.GetValue<string>(APIKEY_HEADER_NAME);
+
+            if (!apikey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            await next();
+        }
+    }
+}
