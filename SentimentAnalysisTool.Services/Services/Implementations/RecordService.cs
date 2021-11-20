84ece7dd6ca332f52e4df4d6ff6a33b0b6016948
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-20 17:48:12 +0800

    Converted RawSql to StoredProcedure(RecordService)

--- SentimentAnalysisTool.Services/Services/Implementations/RecordService.cs
+++ SentimentAnalysisTool.Services/Services/Implementations/RecordService.cs
@@ -11,53 +11,35 @@ namespace SentimentAnalysisTool.Services.Services.Implementations
     {
         public async Task<int> AddRecordAsync(RecordModel record, string connectionString)
         {
-            var sqlQuery = @"INSERT INTO RecordsTable(
-                                RecordName,
-                                PositivePercent,
-                                NegativePercent
-                            )VALUES(
-                                @RecordName,
-                                @PositivePercent,
-                                @NegativePercent
-                            );
-                            SELECT SCOPE_IDENTITY();";
-            using SqlConnection connection = new SqlConnection(connectionString);
+            var procedure = "SaveRecords";
+            using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
             using var transaction = await connection.BeginTransactionAsync();
-            var primaryKey = connection.QuerySingle<int>(sqlQuery, record, transaction);
+            var primaryKey = connection.QuerySingle<int>(procedure, record, transaction, commandType: CommandType.StoredProcedure);
             await transaction.CommitAsync();
             return primaryKey;
         }
 
         public async Task<int> AddRecordAsync(RecordModel record, System.Data.Common.DbTransaction transaction, SqlConnection connection)
         {
-            var sqlQuery = @"INSERT INTO RecordsTable(
-                                RecordName,
-                                PositivePercent,
-                                NegativePercent
-                            )VALUES(
-                                @RecordName,
-                                @PositivePercent,
-                                @NegativePercent
-                            );
-                            SELECT SCOPE_IDENTITY();";
+            var procedure = "SaveRecords";
             if (connection.State == ConnectionState.Closed)
                 await connection.OpenAsync();
 
-            var primaryKey = connection.QuerySingle<int>(sqlQuery, record, transaction);
+            var primaryKey = connection.QuerySingle<int>(procedure, record, transaction, commandType: CommandType.StoredProcedure);
             return primaryKey;
         }
 
         public async Task<bool> DeleteRecordAsync(int recordId, string connectionString)
         {
-            var sqlQuery = @"DELETE FROM RecordsTable WHERE RecordId = @RecordId";
-            using SqlConnection connection = new SqlConnection(connectionString);
+            var procedure = "DeleteRecords";
+            using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
             using var transaction = await connection.BeginTransactionAsync();
-            var rowsAffected = await connection.ExecuteAsync(sqlQuery, new
+            var rowsAffected = await connection.ExecuteAsync(procedure, new
             {
                 RecordId = recordId
-            }, transaction);
+            }, transaction, commandType: CommandType.StoredProcedure);
             await transaction.CommitAsync();
             if (rowsAffected > 0)
                 return true;
@@ -67,11 +49,11 @@ namespace SentimentAnalysisTool.Services.Services.Implementations
 
         public async Task<RecordModel> FindRecordAsync(int recordId, string connectionString)
         {
-            var sqlQuery = @"SELECT * FROM RecordsTable WHERE RecordId = @RecordId";
-            using SqlConnection connection = new SqlConnection(connectionString);
+            var procedure = "FetchRecords";
+            using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
             using var transaction = await connection.BeginTransactionAsync();
-            var record = await connection.QueryFirstOrDefaultAsync<RecordModel>(sqlQuery, new { RecordId = recordId }, transaction);
+            var record = await connection.QueryFirstOrDefaultAsync<RecordModel>(procedure, new { RecordId = recordId }, transaction, commandType: CommandType.StoredProcedure);
             return record;
         }
     }
