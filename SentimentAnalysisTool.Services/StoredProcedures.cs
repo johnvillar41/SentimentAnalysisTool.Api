54d51c25794e72108718754781ae2fcb637f28c9
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-20 18:08:48 +0800

    Added StoredProcedures class

---
+++ SentimentAnalysisTool.Services/StoredProcedures.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services
+{
+    public class StoredProcedures
+    {
+        public const string SP_DELETE_CORPUS_TYPE = "DeleteCorpusType";
+        public const string SP_DELETE_CORPUS_WORD = "DeleteCorpusWord";
+        public const string SP_DELETE_RECORDS = "DeleteRecords";
+        public const string SP_DELETE_SLANG_RECORD = "DeleteSlangRecord";
+
+        public const string SP_FETCH_CORPUS_TYPE = "FetchCorpusType";
+        public const string SP_FETCH_CORPUS_WORD = "FetchCorpusWord";
+        public const string SP_FETCH_RECORDS = "FetchRecords";
+
+        public const string SP_PAGINATE_COMMENTS = "PaginateCommentsTable";
+
+        public const string SP_SAVE_COMMENTS = "SaveComments";
+        public const string SP_SAVE_CORPUS_RECORDS = "SaveCorpusRecords";
+        public const string SP_SAVE_CORPUS_TYPE = "SaveCorpusType";
+        public const string SP_SAVE_CORPUS_WORD = "SaveCorpusWord";
+        public const string SP_SAVE_RECORDS = "SaveRecords";
+        public const string SP_SAVE_SLANG_RECORD= "SaveSlangRecord";
+        public const string SP_SAVE_WORD_FREQUENCY = "SaveWordFrequency";
+    }
+}
