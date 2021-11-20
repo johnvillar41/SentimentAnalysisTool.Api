10614e955f7807b2087f69f7620849d617e6c8ca
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-20 17:28:32 +0800

    Converted RawSql to StoredProcedure(CorpusTypeService) and Updated CorpusTypeService

--- SentimentAnalysisTool.Services/Services/Implementations/CorpusTypeService.cs
+++ SentimentAnalysisTool.Services/Services/Implementations/CorpusTypeService.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using SentimentAnalysisTool.Data.Models;
 using SentimentAnalysisTool.Services.Services.Interfaces;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,32 +11,37 @@ namespace SentimentAnalysisTool.Services.Services.Implementations
     public class CorpusTypeService : ICorpusTypeService
     {
         private readonly ICorpusWordsService _corpusWordsService;
-        public CorpusTypeService(ICorpusWordsService corpusWordsService)
+        private readonly ISlangRecordsService _slangRecordsService;
+        public CorpusTypeService(ICorpusWordsService corpusWordsService, ISlangRecordsService slangRecordsService)
         {
             _corpusWordsService = corpusWordsService;
+            _slangRecordsService = slangRecordsService;
         }
         public async Task<bool> AddCorpusTypeAsync(CorpusTypeModel corpusType, string connectionString)
         {
-            var sqlQuery = @"INSERT INTO CorpusTypeTable(CorpusTypeName)
-                            VALUES(                                
-                                @CorpusTypeName
-                            );
-                            SELECT Scope_Identity();";
+            var procedure = "SaveCorpusType";
             using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
             var transaction = await connection.BeginTransactionAsync();
-            var primaryKey = await connection.QuerySingleAsync<int>(sqlQuery,
+            var primaryKey = await connection.QuerySingleAsync<int>(procedure,
                 new
                 {
                     corpusType.CorpusTypeName
                 },
-                transaction);
+                transaction, commandType: CommandType.StoredProcedure);
 
             if (corpusType.CorpusWords.Any())
             {
                 corpusType.CorpusWords.Select(x => x.CorpusType.CorpusTypeId = primaryKey);
                 var corpusWordResult = await _corpusWordsService.AddCorpusWordsAsync(corpusType.CorpusWords, connectionString);
                 if (!corpusWordResult)
+                    return false;
+            }
+            if (corpusType.SlangRecords.Any())
+            {
+                corpusType.SlangRecords.Select(x => x.CorpusType.CorpusTypeId = primaryKey);
+                var slangRecordResult = await _slangRecordsService.AddSlangRecordAsync(corpusType.SlangRecords, connectionString);
+                if (!slangRecordResult)
                     return false;
             }
 
@@ -48,11 +54,11 @@ namespace SentimentAnalysisTool.Services.Services.Implementations
 
         public async Task<bool> DeleteCorpusTypeAsync(int corpusTypeId, string connectionString)
         {
-            var sqlQuery = @"DELETE FROM CorpusTypeTable WHERE CorpusTypeId = @CorpusTypeId";
+            var procedure = "DeleteCorpusType";
             using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
             using var transaction = await connection.BeginTransactionAsync();
-            var result = await connection.ExecuteAsync(sqlQuery, new { CorpusTypeId = corpusTypeId }, transaction);
+            var result = await connection.ExecuteAsync(procedure, new { CorpusTypeId = corpusTypeId }, transaction, commandType: CommandType.StoredProcedure);
             await transaction.CommitAsync();
             if (result > 0)
                 return true;
@@ -61,11 +67,11 @@ namespace SentimentAnalysisTool.Services.Services.Implementations
 
         public async Task<CorpusTypeModel> FindCorpusTypeAsync(int corpusTypeId, string connectionString)
         {
-            var sqlQuery = @"SELECT * FROM CorpusTypeTable WHERE CorpusTypeId = @CorpusTypeId";
-            using SqlConnection connection = new SqlConnection(connectionString);
+            var Procedure = "FetchCorpusType";
+            using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
             using var transaction = await connection.BeginTransactionAsync();
-            var result = await connection.QueryFirstAsync<CorpusTypeModel>(sqlQuery, new { CorpusTypeId = corpusTypeId }, transaction);
+            var result = await connection.QueryFirstAsync<CorpusTypeModel>(Procedure, new { CorpusTypeId = corpusTypeId }, transaction, commandType: CommandType.StoredProcedure);
             await transaction.CommitAsync();
             return result;
         }
