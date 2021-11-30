b4ed47e5f844559f1e5a04b98230aed4dd70fbab
johnvillar41 <johndanielvillar123@gmail.com>
2021-11-30 19:11:48 +0800

    Added AbbreviationModel

---
+++ SentimentAnalysisTool.Data/Models/AbbreviationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Data.Models
+{
+    public class AbbreviationModel
+    {
+        public int AbbreviationsId { get; set; }
+        public CorpusTypeModel CorpusTypeModel { get; set; }
+        public string Abbreviation { get; set; }
+        public string AbbreviationWord { get; set; }
+    }
+}
---
+++ SentimentAnalysisTool.Services/Services/Interfaces/IAbbreviationsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Services.Services.Interfaces
+{
+    public interface IAbbreviationsService
+    {
+        Task<bool> AddAbbreviationAsync(int recordId,);
+    }
+}
