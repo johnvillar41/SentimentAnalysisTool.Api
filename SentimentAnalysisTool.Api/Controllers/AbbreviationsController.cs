83398c3fbfb4286a91b71871d5737cac6a81931d
johnvillar41 <johndanielvillar123@gmail.com>
2021-12-01 09:28:44 +0800

    Implemented AbbreviationsController

---
+++ SentimentAnalysisTool.Api/Controllers/AbbreviationsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SentimentAnalysisTool.Api.Models;
+using SentimentAnalysisTool.Data.Models;
+using SentimentAnalysisTool.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbbreviationsController : ControllerBase
+    {
+        private readonly IAbbreviationsService _abbreviationsService;
+        private readonly ICorpusTypeService _corpusTypeService;
+        private readonly IConfiguration _configuration;
+        private string ConnectionString { get; }
+        public AbbreviationsController(
+            IAbbreviationsService abbreviationsService,
+            ICorpusTypeService corpusTypeService,
+            IConfiguration configuration)
+        {
+            _abbreviationsService = abbreviationsService;
+            _corpusTypeService = corpusTypeService;
+            _configuration = configuration;
+            ConnectionString = _configuration.GetConnectionString("SentimentDBConnection");
+        }
+        [HttpPost("{recordId}")]
+        public async Task<IActionResult> AddAbbreviations(
+            [FromHeader] int recordId,
+            [FromBody] IEnumerable<AbbreviationsViewModel> abbreviations)
+        {
+            var abbreviationModelTasks = abbreviations.Select(async m => new AbbreviationModel()
+            {
+                AbbreviationsId = -1,
+                CorpusTypeModel = await _corpusTypeService.FindCorpusTypeAsync(m.CorpusTypeId, ConnectionString),
+                Abbreviation = m.Abbreviation,
+                AbbreviationWord = m.AbbreviationWord
+            });
+            var abbreviationModels = await Task.WhenAll(abbreviationModelTasks);
+            var result = await _abbreviationsService.AddAbbreviationAsync(recordId, abbreviationModels, ConnectionString);
+            if (result)
+                return Ok();
+
+            return BadRequest();
+        }
+    }
+}
---
+++ SentimentAnalysisTool.Api/Models/AbbreviationsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Api.Models
+{
+    public class AbbreviationsViewModel
+    {
+        public int CorpusTypeId { get; set; }
+        public string Abbreviation { get; set; }
+        public string AbbreviationWord { get; set; }
+    }
+}
