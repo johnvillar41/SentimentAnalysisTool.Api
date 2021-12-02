c067fc381d8ddea62e6523099536f3bfdfe4b20d
johnvillar41 <johndanielvillar123@gmail.com>
2021-12-02 09:21:06 +0800

    Added SentiWordNetModel

--- SentimentAnalysisTool.Api/Helpers/AlgorithmModels/SentiWordNetModel.cs
+++ SentimentAnalysisTool.Api/Helpers/AlgorithmModels/SentiWordNetModel.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SentimentAnalysisTool.Api.Helpers.AlgorithmModels
 {
     public class SentiWordNetModel
     {
+        private SentimentType _sentimentType;
+
+        [JsonPropertyName("polarityScore")]
+        public double PolarityScore { get; set; }
+
+        [JsonPropertyName("sentimentScore")]
+        public SentimentType Sentiment
+        {
+            get
+            {
+                return _sentimentType;
+            }
+            set
+            {
+                if (value.Equals(nameof(SentimentType.Neutral)))
+                    _sentimentType = SentimentType.Neutral;
+                if (value.Equals(nameof(SentimentType.Negative)))
+                    _sentimentType = SentimentType.Negative;
+                if (value.Equals(nameof(SentimentType.Positive)))
+                    _sentimentType = SentimentType.Positive;
+            }
+        }
     }
 }
