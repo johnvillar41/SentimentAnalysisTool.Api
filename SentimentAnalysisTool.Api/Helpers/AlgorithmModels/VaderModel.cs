8d75a05d947a68ea844b433a0985245439c4064b
johnvillar41 <johndanielvillar123@gmail.com>
2021-12-02 09:24:34 +0800

    Added Attributes on VaderModel

--- SentimentAnalysisTool.Api/Helpers/AlgorithmModels/VaderModel.cs
+++ SentimentAnalysisTool.Api/Helpers/AlgorithmModels/VaderModel.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SentimentAnalysisTool.Api.Helpers
 {
     public class VaderModel
     {
+        [JsonPropertyName("compoundScore")]
         public SentimentType Sentiment { get; set; }
+
+        [JsonPropertyName("compoundValue")]
         public double CompoundValue { get; set; }
+
+        [JsonPropertyName("negativeValue")]
         public double NegativeValue { get; set; }
+
+        [JsonPropertyName("neutralValue")]
         public double NeutralValue { get; set; }
+
+        [JsonPropertyName("positiveValue")]
         public double PositiveValue { get; set; }
     }
 }
