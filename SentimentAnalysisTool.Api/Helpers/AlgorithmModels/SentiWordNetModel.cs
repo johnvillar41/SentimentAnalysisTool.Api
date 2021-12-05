d5a965c759a55827bcc15ccb3d925fd098b24185
johnvillar41 <johndanielvillar123@gmail.com>
2021-12-05 12:15:59 +0800

    Updated SentiWordNetModel

--- SentimentAnalysisTool.Api/Helpers/AlgorithmModels/SentiWordNetModel.cs
+++ SentimentAnalysisTool.Api/Helpers/AlgorithmModels/SentiWordNetModel.cs
@@ -11,6 +11,12 @@ namespace SentimentAnalysisTool.Api.Helpers.AlgorithmModels
         [JsonPropertyName("polarityScore")]
         public double PolarityScore { get; set; }
 
+        [JsonPropertyName("positiveScore")]
+        public double PositiveScore { get; set; }
+
+        [JsonPropertyName("negativeScore")]
+        public double NegativeScore { get; set; }
+
         [JsonPropertyName("sentimentScore")]
         public string SentimentScore { get; set; }
     }
