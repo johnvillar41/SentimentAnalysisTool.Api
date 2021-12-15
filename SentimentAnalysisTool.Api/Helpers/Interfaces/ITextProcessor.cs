eec01ed9c3fc841c2cd0e76f4cb5c54056f70d0c
johnvillar41 <johndanielvillar123@gmail.com>
2021-12-15 14:11:43 +0800

    Added ITextProcessor

---
+++ SentimentAnalysisTool.Api/Helpers/Interfaces/ITextProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisTool.Api.Helpers.Interfaces
+{
+    public interface ITextProcessor
+    {
+        Task<string> RemoveSpecialCharsAsync(string comment, int totalChars);
+        Task<string> RemoveSlangWordAsync(string comment);
+        Task<string> ReplaceAbbreviationAsync(string comment);
+    }
+}
