17f5fec9bb5e220bdf59584a3f48d573d70cd4a1
johnvillar41 <johndanielvillar123@gmail.com>
2022-01-08 17:48:39 +0800

    Added Attributes on WordFrequencyViewModel

--- SentimentAnalysisTool.Api/Models/WordFrequencyViewModel.cs
+++ SentimentAnalysisTool.Api/Models/WordFrequencyViewModel.cs
@@ -2,16 +2,26 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SentimentAnalysisTool.Api.Models
 {
     public class WordFrequencyViewModel
     {
+        [JsonPropertyName("wordFrequencyId")]
         public int WordFrequencyId { get; set; }
+
+        [JsonPropertyName("recordId")]
         public int RecordId { get; set; }
+
+        [JsonPropertyName("word")]
         public string Word { get; set; }
+
+        [JsonPropertyName("wordFrequency")]
         public int WordFrequency { get; set; }
+
+        [JsonPropertyName("wordType")]
         public string WordType { get; set; }
         public WordFrequencyViewModel(WordFrequencyModel wordFrequencyModel)
         {
